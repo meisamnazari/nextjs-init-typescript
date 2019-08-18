9c839a1449e1f7428cfc28577ad6666acb9d8b15
meisam <meisamnazari71@gmail.com>
2019-08-18 16:25:09 +0430

    first commit

---
+++ pages/user.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor:'red'}}>
+                Gajinooooooooooooooooooooooooooo
+            </div>
+        );
+    }
+}
+
+export default User;
